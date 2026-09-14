f2d8c589837d50945a319442f3740bf56592f31e
marinxzrs <mariane.lemes.oliveira@escola.pr.gov.br>
2026-09-14 13:51:13 +0000

    aula5

--- main.js
+++ main.js
@@ -49,5 +49,14 @@ let perguntaAtual;
 function mostraPergunta() {
   perguntaAtual = perguntas[atual];
   caixaPerguntas.textContent = perguntaAtual.enunciado;
+  mostraAlternativas();
 }
+function mostraAlternativas() {
+  for (const alternativa of perguntaAtual.alternativas) {
+    const botaoAlternativas = document.createElement("button");
+    botaoAlternativas.textContent = alternativa;
+    caixaAlternativas.appendChild(botaoAlternativas);
+  }
+}
+
 mostraPergunta();
